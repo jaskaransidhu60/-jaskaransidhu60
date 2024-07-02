af559c57ce8779bf29c45f9816b01a58ffffb29f
jaskaran singh sidhu <sidhuja@oregonstate.edu>
2024-07-01 20:17:57 -0700

    Update and rename output.txt to hello_gradescope.py

---
+++ hello_gradescope.py
@@ -0,0 +1,8 @@
+# Name: Jaskarans Singh Sidhu
+# OSU Email: sidhuja@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment:GradeScope Submission
+# Due Date: 7/1/2024
+# Description: This is the assignment to connect with Gradescope and verify that the setup is working correctly.
+def gradescope_test():
+return 42
